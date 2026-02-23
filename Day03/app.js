b02325aa3120022cedfbe2f842dda28782bda592
priyansh1224 <priyanshuoli30@gmail.com>
2026-02-23 11:14:31 +0530

    DAY04 complted successfully

--- Day03/app.js
+++ Day03/app.js
@@ -1,12 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
 import 'dotenv/config';
-import readlineSync from 'readline-sync';
-
+import readLineSYnc from readLineSYnc;
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
-while(true){
-  const question = readlineSync.question("Ask anything related to the coding? ");
-  
+async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     systemInstruction:`you are a coding tutor,
@@ -15,8 +12,41 @@ while(true){
     -dont answer anything which is not relted to coding
     -React rudely to ser if they ask question which is not relted to coding
     Ex: You dumb, only ask question relted to the coding`,
-    contents: question
+    contents:"what is array explain in few words"
   });
-  
   console.log("Response:", response.text);
 }
+
+while(true){
+  const question 
+  = readLineSYnc.question("Ask anything related to the coding? ");
+   if(question=="exit"){
+    break;
+  }
+  const response=await chat.sendMessage({
+    message:question
+  })
+  console.log("Response:", response.text);
+}
+
+await main();
+
+// const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+  //   config:{
+  //       systemInstruction:`you are a coding tutor,
+  //       Strict Role to follow 
+  //       -you will only anser the question which relted to the coding
+  //       -dont answer anything which is not relted to coding
+  //       -React rudely to ser if they ask question which is not relted to coding
+  //       Ex: You dumb, only ask question relted to the coding`,
+  // },
+  //   contents:"what is array"
+  // });
+//   console.log(response.text);
+// }
+
+// await main();
